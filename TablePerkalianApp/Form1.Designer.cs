9fb6bdb080040b456eb429948069b99514df2c60
Erik Gunawan <erik.gunawan44@gmail.com>
2014-10-14 13:59:09 +0700

    Table Perkalian DataGridView

---
+++ TablePerkalianApp/Form1.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace TablePerkalianApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBaris = new System.Windows.Forms.TextBox();
+            this.txtKolom = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnProses = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnKeluar = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.gridTable = new System.Windows.Forms.DataGridView();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.lblTable = new System.Windows.Forms.Label();
+            this.lblKolom = new System.Windows.Forms.Label();
+            this.lblBaris = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gridTable)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Baris:";
+            // 
+            // txtBaris
+            // 
+            this.txtBaris.Location = new System.Drawing.Point(17, 48);
+            this.txtBaris.Name = "txtBaris";
+            this.txtBaris.Size = new System.Drawing.Size(150, 20);
+            this.txtBaris.TabIndex = 1;
+            // 
+            // txtKolom
+            // 
+            this.txtKolom.Location = new System.Drawing.Point(17, 96);
+            this.txtKolom.Name = "txtKolom";
+            this.txtKolom.Size = new System.Drawing.Size(150, 20);
+            this.txtKolom.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Kolom:";
+            // 
+            // btnProses
+            // 
+            this.btnProses.Location = new System.Drawing.Point(203, 36);
+            this.btnProses.Name = "btnProses";
+            this.btnProses.Size = new System.Drawing.Size(82, 32);
+            this.btnProses.TabIndex = 5;
+            this.btnProses.Text = "Proses";
+            this.btnProses.UseVisualStyleBackColor = true;
+            this.btnProses.Click += new System.EventHandler(this.btnProses_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnKeluar);
+            this.groupBox1.Controls.Add(this.btnProses);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtBaris);
+            this.groupBox1.Controls.Add(this.txtKolom);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(26, 25);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(305, 139);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Action";
+            // 
+            // btnKeluar
+            // 
+            this.btnKeluar.Location = new System.Drawing.Point(203, 84);
+            this.btnKeluar.Name = "btnKeluar";
+            this.btnKeluar.Size = new System.Drawing.Size(82, 32);
+            this.btnKeluar.TabIndex = 6;
+            this.btnKeluar.Text = "Keluar";
+            this.btnKeluar.UseVisualStyleBackColor = true;
+            this.btnKeluar.Click += new System.EventHandler(this.btnKeluar_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.gridTable);
+            this.groupBox2.Location = new System.Drawing.Point(26, 188);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(598, 279);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Output";
+            // 
+            // gridTable
+            // 
+            this.gridTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridTable.Location = new System.Drawing.Point(17, 37);
+            this.gridTable.Name = "gridTable";
+            this.gridTable.Size = new System.Drawing.Size(553, 226);
+            this.gridTable.TabIndex = 0;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.lblTable);
+            this.groupBox3.Controls.Add(this.lblKolom);
+            this.groupBox3.Controls.Add(this.lblBaris);
+            this.groupBox3.Controls.Add(this.label6);
+            this.groupBox3.Controls.Add(this.label7);
+            this.groupBox3.Controls.Add(this.label8);
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Location = new System.Drawing.Point(352, 25);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(272, 139);
+            this.groupBox3.TabIndex = 8;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Keterangan";
+            // 
+            // lblTable
+            // 
+            this.lblTable.AutoSize = true;
+            this.lblTable.Location = new System.Drawing.Point(104, 91);
+            this.lblTable.Name = "lblTable";
+            this.lblTable.Size = new System.Drawing.Size(44, 13);
+            this.lblTable.TabIndex = 8;
+            this.lblTable.Text = "lblTable";
+            // 
+            // lblKolom
+            // 
+            this.lblKolom.AutoSize = true;
+            this.lblKolom.Location = new System.Drawing.Point(104, 64);
+            this.lblKolom.Name = "lblKolom";
+            this.lblKolom.Size = new System.Drawing.Size(46, 13);
+            this.lblKolom.TabIndex = 7;
+            this.lblKolom.Text = "lblKolom";
+            // 
+            // lblBaris
+            // 
+            this.lblBaris.AutoSize = true;
+            this.lblBaris.Location = new System.Drawing.Point(104, 37);
+            this.lblBaris.Name = "lblBaris";
+            this.lblBaris.Size = new System.Drawing.Size(40, 13);
+            this.lblBaris.TabIndex = 6;
+            this.lblBaris.Text = "lblBaris";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(96, 90);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(10, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = ":";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(96, 63);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(10, 13);
+            this.label7.TabIndex = 4;
+            this.label7.Text = ":";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(96, 36);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(10, 13);
+            this.label8.TabIndex = 3;
+            this.label8.Text = ":";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(17, 91);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(81, 13);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Tabel Perkalian";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 64);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Jumlah Kolom";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Jumlah Baris";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(648, 495);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Membuat Table Perkalian Dasar di DataGridView";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.gridTable)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBaris;
+        private System.Windows.Forms.TextBox txtKolom;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnProses;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnKeluar;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView gridTable;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblTable;
+        private System.Windows.Forms.Label lblKolom;
+        private System.Windows.Forms.Label lblBaris;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ TablePerkalianApp/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TablePerkalianApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnProses_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int baris = Convert.ToInt32(txtBaris.Text);
+                int kolom = Convert.ToInt32(txtKolom.Text);
+
+                if (baris >= 0 && kolom >= 0)
+                {
+                    gridTable.Rows.Clear();
+                    gridTable.Columns.Clear();
+                    gridTable.ColumnCount = kolom + 1;
+
+                    // Menampilkan table perkalian di DataGridView
+                    // Menggunakan Array 1 dimensi
+                    /*
+                    string[] arr;
+                    for (int i = 0; i <= baris; i++)
+                    {
+                        arr = new string[kolom + 1];
+                        for (int j = 0; j <= kolom; j++)
+                        {
+                            arr[j] = (i * j).ToString();
+
+                            gridTable.Columns[j].Name = j.ToString();
+
+                        }
+                        gridTable.Rows.Add(arr);
+                        gridTable.Rows[i].HeaderCell.Value = Convert.ToString(i);
+                    }
+                    */
+                    // End
+
+                    // Menampilkan table perkalian di DataGridView
+                    // Menggunakan Array 2 dimensi
+                    string[,] arr = new string[baris + 1, kolom + 1];
+
+                    int row = arr.GetLength(0);
+                    int column = arr.GetLength(1);
+
+                    for (int i = 0; i < row; i++)
+                    {
+                        var rows = new DataGridViewRow();
+
+                        for (int j = 0; j < column; j++)
+                        {
+                            arr[i, j] = (i * j).ToString();
+
+                            rows.Cells.Add(new DataGridViewTextBoxCell()
+                            {
+                                Value = arr[i, j]
+                            });
+
+                            gridTable.Columns[j].Name = j.ToString();
+                        }
+
+                        gridTable.Rows.Add(rows);
+                        gridTable.Rows[i].HeaderCell.Value = Convert.ToString(i);
+                    }
+                    // End
+
+                    lblBaris.Text = (baris + 1).ToString();
+                    lblKolom.Text = (kolom + 1).ToString();
+                    lblTable.Text = "0 x 0 sampai " + baris + " x " + kolom;
+                }
+                else
+                    MessageBox.Show("Masukkan bilangan cacah (lebih dari atau sama dengan nol)!", "Pesan Kesalahan",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (FormatException fe)
+            {
+                MessageBox.Show("Karakter yang Anda masukkan salah! \nHanya masukkan angka!", "Pesan Kesalahan",
+                    MessageBoxButtons.OK ,MessageBoxIcon.Error);
+            }
+        }
+
+
+        private void btnKeluar_Click(object sender, EventArgs e)
+        {
+            Dispose();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            reset("");
+
+            gridTable.AllowUserToAddRows = false;
+            gridTable.ReadOnly = true;
+            this.gridTable.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            
+        }
+
+        private void reset(String s)
+        {
+            lblBaris.Text = s;
+            lblKolom.Text = s;
+            lblTable.Text = s;
+        }
+    }
+}
